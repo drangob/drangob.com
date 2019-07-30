839a05debb83319309571d27e1dd9c8876aa69eb
Daniel Horbury <dhorbury@mintel.com>
2019-07-30 14:46:41 +0000

    Updates

---
+++ precache-manifest.c6bf6d5ade109de56975c759f4f8bf47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea044205ae1dc41b8ed3f77baa705fc",
+    "url": "drangob.com/index.html"
+  },
+  {
+    "revision": "490727f4c2429c031cb2",
+    "url": "drangob.com/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "drangob.com/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "490727f4c2429c031cb2",
+    "url": "drangob.com/static/js/main.f1eb19bd.chunk.js"
+  },
+  {
+    "revision": "5683348af812a12c8ead",
+    "url": "drangob.com/static/js/runtime~main.e48555cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "drangob.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "drangob.com/precache-manifest.c6bf6d5ade109de56975c759f4f8bf47.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("drangob.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
